94a52d766be12379e935474ccab90c481a6b0cec
andres <andres@ip-192-168-24-71.ec2.internal>
2018-04-09 17:51:34 -0600

    wip

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyACOimZRn0kH2sG6cSVpCboik-_2AiFavY",
+    authDomain: "hilleducation-35f3a.firebaseapp.com",
+    databaseURL: "https://hilleducation-35f3a.firebaseio.com",
+    projectId: "hilleducation-35f3a",
+    storageBucket: "hilleducation-35f3a.appspot.com",
+    messagingSenderId: "196436984639"
+  };
+var fire = firebase.initializeApp(config);
+export default fire;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import fire from '../fire';
 
-const Login = () => 
-  <div>
-    <input />
-    <input />
-  </div>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { accounts: [] }; // <- set up react state
+  }
+  componentWillMount(){
+    /* Create reference to accounts in Firebase Database */
+    let accountsRef = fire.database().ref('accounts').orderByKey().limitToLast(100);
+    accountsRef.on('child_added', snapshot => {
+      /* Update React state when account is added at Firebase Database */
+      let account = { email: snapshot.val().email, passwordChanged: snapshot.val().passwordChanged, id: snapshot.key };
+      this.setState({ accounts: [account].concat(this.state.accounts) });
+    })
+  }
+  addAccount(e){
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    /* Send the account to Firebase */
+    const tempPass = this.uuidv4();
+    fire.database().ref('accounts').push( { email: this.inputEl.value, pass: tempPass, passwordChanged: false } );
+    this.inputEl.value = ''; // <- clear the input
+  }
+
+  uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.addAccount.bind(this)}>
+          <input type="text" ref={ el => this.inputEl = el }/>
+          <input type="submit"/>
+          <ul>
+            { /* Render the list of accounts */
+              this.state.accounts.map( account => <li key={account.id}>{account.email}</li> )
+            }
+          </ul>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;

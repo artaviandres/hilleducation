12c0231eef2cd2680fb57e6f99e53d35eb5c2d5a
Andrés Artavia <andresartavia@Andress-MacBook-Pro.local>
2018-05-10 22:29:37 -0600

    wip

--- src/reducers/admins.js
+++ src/reducers/admins.js
@@ -1,5 +1,3 @@
-// andrés, priscila, cristopher, other guy
-
-export default(state = ['test', 'priscila', 'cristopher']) => {
+export default(state = ['gA7W2qyWPgTXSKy23SUTUH2FcLt1', 'VmH5ZEBJ2dYGtlCz9NBWcb6t5Z62', 'cristopher']) => {
   return state;
 }
